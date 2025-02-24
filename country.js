515fcc4641e4c005176410075d95a50c8a4d7fb5
thomaswalsh86 <walshthomas530@gmail.com>
2025-02-24 20:31:55 +0000

    Country for some men who would like to know about a country

---
+++ country.js
@@ -0,0 +1,25 @@
+function getCountry() {
+    var input = document.getElementById("cInput").value.trim();
+    
+    if (input === "") {
+        document.getElementById("cy").innerText = "Please enter a country name.";
+        return;
+    }
+    input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+
+    fetch(`https://countries-api-abhishek.vercel.app/countries/${input}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Country not found");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        document.getElementById("cy").innerText = "Country: " + data.data.name;
+        console.log("Country is: " + data.data.name);
+    })
+    .catch(error => {
+        document.getElementById("cy").innerText = "Error: " + error.message;
+        console.error("Error fetching country:", error);
+    });
+}
